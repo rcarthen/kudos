441cfc234671cdf9c4073fee454a0525210421cc
Ruth <rcarthen@gmail.com>
2018-11-16 03:11:25 -0600

    frontend styling

---
+++ models/seeder.js
@@ -0,0 +1,23 @@
+Users 
+
+Peace;
+Love;
+Hope;
+Faith;
+
+
+
+Affirmations 
+"Clarity"
+"I wake up today with strength in my heart and clarity in my mind."
+
+"Goodness"
+"Everything that is happening now is happening for my ultimate good."
+
+"Happiness"
+"Happiness is a choice. I base my happiness on my own accomplishments and the blessings I've been given."
+
+"Creativity"
+"Creative energy surges through me and leads me to new and brilliant ideas."
+
+
